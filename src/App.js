2340e44f734a330692a69661206b9fa2d3e46af0
kirubelM <kirubelmognehode18@augustana.edu>
2023-05-10 22:55:34 -0500

    added routes, app.css and basic layout foundations

--- src/App.js
+++ src/App.js
@@ -1,7 +1,43 @@
 import React from "react";
-
+import { Route, Link, Routes } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+import {
+  Navbar,
+  Exchanges,
+  Homepage,
+  Cryptocurrencies,
+  CryptoDetails,
+  News,
+} from "./components";
+import "./App.css";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route exact path="/Homepage" element={<Homepage />}></Route>
+              <Route exact path="/exchanges" element={<Exchanges />}></Route>
+              <Route
+                exact
+                path="/cryptocurrencies"
+                element={<Cryptocurrencies />}></Route>
+              <Route
+                exact
+                path="/crypto/:coinId"
+                element={<CryptoDetails />}></Route>
+              <Route exact path="/news" element={<News />}></Route>
+            </Routes>
+          </div>
+        </Layout>
+      </div>
+      <div className="footer"></div>
+    </div>
+  );
 };
 
 export default App;
