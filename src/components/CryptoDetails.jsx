42dca60f15d68f07cb17eac72a1430bdace90bfc
kirubelM <kirubelmognehode18@augustana.edu>
2023-05-23 17:29:13 -0500

    Mobile friendly updates

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -1,29 +1,30 @@
-import React, { useState } from 'react';
+import React from 'react';
 import HTMLReactParser from 'html-react-parser';
 import { useParams } from 'react-router-dom';
 import millify from 'millify';
-import { Col, Row, Typography, Select } from 'antd';
+import { Col, Row, Typography } from 'antd';
 import { MoneyCollectOutlined, DollarCircleOutlined, FundOutlined, ExclamationCircleOutlined, StopOutlined, TrophyOutlined, CheckOutlined, NumberOutlined, ThunderboltOutlined } from '@ant-design/icons';
 
-import { useGetCryptoDetailsQuery, useGetCryptoHistoryQuery } from '../services/cryptoApi';
+import { useGetCryptoDetailsQuery } from '../services/cryptoApi';
+// import {  useGetCryptoHistoryQuery } from '../services/cryptoApi';
 // import Loader from './Loader';
-import LineChart from './LineChart';
+// import LineChart from './LineChart';
 
 const { Title, Text } = Typography;
-const { Option } = Select;
+// const { Option } = Select;
 
 const CryptoDetails = () => {
   const { coinId } = useParams();
-  const [timeperiod, setTimeperiod] = useState('7d');
+  // const [timeperiod, setTimeperiod] = useState('7d');
   const { data, isFetching } = useGetCryptoDetailsQuery(coinId);
-  const { data: coinHistory } = useGetCryptoHistoryQuery({ coinId, timeperiod });
+  // const { data: coinHistory } = useGetCryptoHistoryQuery({ coinId, timeperiod });
   const cryptoDetails = data?.data?.coin;
 
   if (isFetching) return "Loading...";
   if (!cryptoDetails) {
     return <div>Loading...</div>;
   }
-  const time = ['3h', '24h', '7d', '30d', '1y', '3m', '3y', '5y'];
+  // const time = ['3h', '24h', '7d', '30d', '1y', '3m', '3y', '5y'];
 
   const stats = [
     { title: 'Price to USD', value: `$ ${cryptoDetails?.price && millify(cryptoDetails?.price)}`, icon: <DollarCircleOutlined /> },
